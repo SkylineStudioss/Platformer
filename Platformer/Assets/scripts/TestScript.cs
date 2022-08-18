04273839f6493b68f6b6466908304aab966efa8a
Calan <Calzone407@gmail.com>
2022-08-18 14:49:03 -0700

    Test_Commit
    Added scripts folder and Test_Script and Test_Object

---
+++ Platformer/Assets/scripts/TestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public int Number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Number++;
+        Debug.Log("Your Number is " + Number);
+    }
+}
